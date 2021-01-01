8d614273f7f264199909deb90c5d863b8947a35b
Charlie Stanley <charlie.stanley@microsoft.com>
2020-12-31 17:33:23 -0800

    Add --version option to mm.py

--- mechanical_markdown/__init__.py
+++ mechanical_markdown/__init__.py
@@ -4,9 +4,9 @@ Copyright (c) Microsoft Corporation.
 Licensed under the MIT License.
 """
 
-__version__ = '0.1.7'
-
 from mechanical_markdown.recipe import Recipe as MechanicalMarkdown
 from mechanical_markdown.parsers import MarkdownAnnotationError
+
+__version__ = '0.1.8'
 
 __all__ = [MechanicalMarkdown, MarkdownAnnotationError]
