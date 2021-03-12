d1d4fbf4a54c8791467920196a909b06f38016e1
Charlie Stanley <charlie.stanley@microsoft.com>
2021-03-12 12:24:50 -0800

    Missing requests dependancy

--- mechanical_markdown/__init__.py
+++ mechanical_markdown/__init__.py
@@ -7,6 +7,6 @@ Licensed under the MIT License.
 from mechanical_markdown.recipe import Recipe as MechanicalMarkdown
 from mechanical_markdown.parsers import MarkdownAnnotationError
 
-__version__ = '0.2.5'
+__version__ = '0.2.6'
 
 __all__ = [MechanicalMarkdown, MarkdownAnnotationError]
